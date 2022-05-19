a6168b16181c6e1a699fb54b9ff866bd9776523e
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-05-19 16:09:54 +0800

    add some functionalities

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 
 const ProductRoute = require("./routes/Products");
 const UsersRoute = require("./routes/UsersRoute");
+const { urlencoded } = require("express");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(urlencoded({ extended: false }));
 
 app.use("/users", UsersRoute);
 app.use("/products", ProductRoute);
